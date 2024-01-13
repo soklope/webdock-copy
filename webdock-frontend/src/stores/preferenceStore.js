12ff65341003876ecb47e8d0c388beb0a044ac4e
KasperPtak <kasperptak2@gmail.com>
2024-01-13 19:41:27 +0100

    updated preference store to have a single dynamic fetch for all prefs

--- webdock-frontend/src/stores/preferenceStore.js
+++ webdock-frontend/src/stores/preferenceStore.js
@@ -1,5 +1,32 @@
 import create from 'zustand';
 
+const updateSettingInDatabase = async (settingName, value) => {
+    try {
+      const user = localStorage.getItem("user");
+      const parsedUser = user ? JSON.parse(user) : null;
+  
+      const response = await fetch(`${window.apiHostName}/v1/users/${parsedUser.id}/settings`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ settingName, value}),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(`succesfully updated ${settingName} to ${value} `);
+      } else { 
+        const errorData = await response.json();
+        console.error(errorData.error);
+        // Handle error appropriately
+      }
+  
+    } catch (error) {
+      console.error(`Error updating ${settingName} on the backend:`, error);
+    }
+  };
+
 const usePreferenceStore = create((set) => ({
   theme: localStorage.getItem('theme') || 'light',
   layout: 'standard', // TODO: option to add my post to roadmap view
@@ -8,33 +35,8 @@ const usePreferenceStore = create((set) => ({
     set({ theme: newTheme });
     localStorage.setItem('theme', newTheme);
 
-    // update the theme in database/backend
-    try {
-        const user = localStorage.getItem("user");
-        const parsedUser = user ? JSON.parse(user) : null;
-        
-        const response = await fetch(`${window.apiHostName}/v1/users/${parsedUser.id}/settings`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ theme: newTheme }),
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data.message, data.theme);
-        } else { 
-            const errorData = await response.json();
-            console.error(errorData.error)
-            // console.log("Error", response.status, response.statusText)
-        }
-        
-      } catch (error) {
-        console.error('Error updating theme on the backend:', error);
-        // revert the theme in the local state? since error
-      }
-  },
+    await updateSettingInDatabase('theme', newTheme);
+},
 
   setLayout: (newLayout) => {
     set({ layout: newLayout });
